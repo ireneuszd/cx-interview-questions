15256c5019c16006c106d4a9c9673574cf89ca89
ireneuszd <34318556+ireneuszd@users.noreply.github.com>
2021-03-21 22:03:18 +0100

    Update basket.py

--- shopping_basket/basket.py
+++ shopping_basket/basket.py
@@ -1,30 +1,38 @@
-
-
 class CatalogueException(Exception):
-    """Exception raised for errors in the catalogue"""
-    def __init__(self, product):
-        self.message = f"Product: '{product}' does not exist in catalogue"
-        super().__init__(self.message)
+    """Exception raised for errors in the Catalogue"""
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class BasketException(Exception):
+    """Exception raised for errors in the Basket"""
+    def __init__(self, message):
+        super().__init__(message)
 
 
 class Basket:
     def __init__(self, name, catalogue, offers):
         self.__name = name
         self.__products = []
+        self.__products_distinct = {}
         self.subtotal = 0
         self.discount = 0
         self.total = 0
-        self.products_distinct = {}
         self.offers = offers
         self.catalogue = catalogue
 
-    def get_basket_prices(self):
+    def get_basket_prices(self, pennies=False):
         """Returns subtotal, discount and total prices in given order
 
+        :param pennies: True if price should be return in pennies
+        :type pennies: bool
         :returns tuple of prices
         :return type: tuple
         """
-        return self.subtotal, self.discount, self.total
+        divider = 1 if pennies else 100
+        return (self.subtotal / divider,
+                self.discount / divider,
+                self.total / divider)
 
     def set_products(self, products):
         """Sets products for the basket
@@ -33,6 +41,8 @@ class Basket:
         :type products: list
         """
         self.__products.clear()
+        self.__products_distinct.clear()
+
         for product in products:
             self.check_if_product_in_catalogue(product)
             self.__products.append(product)
@@ -45,24 +55,23 @@ class Basket:
 
         for product in self.__products:
             self.subtotal += self.get_price(product)
-            if product not in self.products_distinct:
-                self.products_distinct[product] = 1
+            if product not in self.__products_distinct:
+                self.__products_distinct[product] = 1
             else:
-                self.products_distinct[product] += 1
+                self.__products_distinct[product] += 1
 
         self.count_discount()
         self.total = self.subtotal - self.discount
 
     def count_discount(self):
         """Counts total discount based on basket content"""
-        for product in self.products_distinct:
-            tmp_amount = self.products_distinct[product]
+        for product in self.__products_distinct:
+            tmp_amount = self.__products_distinct[product]
             if product in self.offers:
                 price = self.get_price(product)
                 for offer in self.offers[product]:
                     while tmp_amount >= offer['amount']:
                         self.discount += round(offer['discount'](price))
-                        print(self.discount)
                         tmp_amount -= offer['amount']
 
     def get_price(self, product):
@@ -82,6 +91,4 @@ class Basket:
         :raises CatalogueException: if product does not exist in catalogue
         """
         if product not in self.catalogue.keys():
-            raise CatalogueException(product)
-
-
+            raise CatalogueException(f"{product} not in Catalogue")
